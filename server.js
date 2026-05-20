8ad0a791687fa294147feb0764fcb8046c4c069c
Jonathan Marques <marquessent@gmail.com>
2026-05-20 20:42:37 -0300

    giro-solidario-backend

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ cron.schedule('* * * * *', () => {
 })
 
 // Executa diariamente à meia-noite (00:00)
-cron.schedule('* * * * *', () => {
+cron.schedule('0 0 * * *', () => {
   console.log(
     '⏰ [CRON-DIARIO] Executando limpeza de vermelhos inadimplentes...'
   )
